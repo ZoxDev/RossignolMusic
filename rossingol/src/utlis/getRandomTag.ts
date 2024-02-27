c6bb08671bd25d5ece0d11753ff9dfd400ff4dfc
ZoxxxDev <88402006+ZoxDev@users.noreply.github.com>
2024-02-27 17:57:30 +0100

    Changing type for typesafety

--- rossingol/src/utlis/getRandomTag.ts
+++ rossingol/src/utlis/getRandomTag.ts
@@ -5,12 +5,12 @@ const getRandomTag = async () => {
     const getAllThePages = await fetch(`http://ws.audioscrobbler.com/2.0/?method=chart.gettoptags&api_key=${API_KEY}&format=json`).then(res => res.json());
 
     // Get a random page in result
-    const randomPage = Math.floor(Math.random() * getAllThePages.tags['@attr'].totalPages) + 1;
+    const randomPage : number = Math.floor(Math.random() * getAllThePages.tags['@attr'].totalPages) + 1;
 
     // Get a random tag in the page
     const getTagsOnPage = await fetch(`http://ws.audioscrobbler.com/2.0/?method=chart.gettoptags&api_key=${API_KEY}&format=json&page=${randomPage}`).then(res => res.json());
-    const getRandomTagIndex = Math.floor(Math.random() * getTagsOnPage.tags.tag.length);
-    const tagName = getTagsOnPage.tags.tag[getRandomTagIndex].name;
+    const getRandomTagIndex : number = Math.floor(Math.random() * getTagsOnPage.tags.tag.length);
+    const tagName : string = getTagsOnPage.tags.tag[getRandomTagIndex].name;
 
     return console.log(tagName);
 }
