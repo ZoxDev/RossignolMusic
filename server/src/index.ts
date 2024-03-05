9cfef4f084113cc2f12c9af045331fcdcd51994b
ZoxDev <mathias.bar1809@gmail.com>
2024-03-05 10:13:31 +0100

    Add : youtubei.js to the project

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,14 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
-const app = new Hono()
+const app = new Hono();
+app.use(cors());
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
 
+const port = 3000;
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
