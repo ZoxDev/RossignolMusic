b5b1014e796df929a6e8f1bb0fea4e0e23da356e
ZoxDev <mathias.bar1809@gmail.com>
2024-03-05 10:16:45 +0100

    Add : getvideo function, with /getVideo/{param}

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
+import getVideo from './getVideo';
 
 const app = new Hono();
 app.use(cors());
 
-
+app.get('/getVideo/', getVideo);
 
 const port = 3000;
 serve({
