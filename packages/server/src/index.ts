b55f36194f1a2f4b7a4a9e996ebb2573c13ea855
ZoxDev <mathias.bar1809@gmail.com>
2024-03-07 17:43:53 +0100

    refactor: move to single file

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,15 +1,25 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
-import getVideo from './getVideo';
+import { Innertube } from 'youtubei.js';
 
 const app = new Hono();
 app.use(cors());
 
-app.get('/getVideo/', getVideo);
+app.get('/getVideo/', async (c) => {
+  const yt = await Innertube.create();
+
+  const { artist, title } = c.req.query();
+  const video = await yt.search(`${artist} ${title}`);
+
+  return c.json(video);
+});
 
 const port = 3000;
-serve({
-  fetch: app.fetch,
-  port,
-});
+serve(
+  {
+    fetch: app.fetch,
+    port,
+  },
+  () => console.log('server started')
+);
