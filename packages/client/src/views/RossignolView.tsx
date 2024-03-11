c773c2878d51f7779a73afe926cc1c72af059f60
ZoxDev <mathias.bar1809@gmail.com>
2024-03-11 16:28:30 +0100

    feat: on change of tracklist state change music

--- packages/client/src/views/RossignolView.tsx
+++ packages/client/src/views/RossignolView.tsx
@@ -1,8 +1,11 @@
 import useTags from '../hooks/useTags';
 import useTracks from '../hooks/useTracks';
 import useRandom from '../hooks/useRandom';
-import { useState } from 'react';
 import useSearchSong from '../hooks/useSearchSong';
+import useTrackList from '../hooks/useTrackList';
+
+import { useState } from 'react';
+
 import Player from '../components/Player';
 
 const MAX_TAG_PAGE = 2000;
@@ -16,8 +19,8 @@ const RossignolView = () => {
   const tracks = useTracks(tag?.name, randomTracksPage);
   const track = useRandom(tracks.data?.tracks.track);
   const songQuery = useSearchSong(track);
+  const { trackList, handleDeleteTrack } = useTrackList(songQuery.data);
 
-  // Click action
   const handleGetRandomTrack = () => {
     setRandomTagsPage(Math.floor(Math.random() * MAX_TAG_PAGE));
     setRandomTracksPage(
@@ -34,16 +37,19 @@ const RossignolView = () => {
   return (
     <>
       <button onClick={handleGetRandomTrack}>RANDOM TRACK</button>
-      <button>RANDOM SIMILAR TRACK</button>
       <div>
         {tags.isLoading || tracks.isLoading ? (
           <h1>Loading...</h1>
         ) : (
           <>
-            {songQuery.isLoading || songQuery.isError ? (
+            {songQuery.isLoading || songQuery.isError || trackList.length === 0 ? (
               <h1>Loading...</h1>
             ) : (
-              <Player song={songQuery.data} handleNext={handleGetRandomTrack} />
+              <Player
+                song={trackList[trackList.length - 1]}
+                handlePrev={handleDeleteTrack}
+                handleNext={handleGetRandomTrack}
+              />
             )}
           </>
         )}
