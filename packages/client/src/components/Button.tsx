d4fbcc8453dab653dc25acc6b3c3822d59408600
ZoxDev <mathias.bar1809@gmail.com>
2024-03-20 14:02:36 +0100

    feat: button can get svg and modify few style

--- packages/client/src/components/Button.tsx
+++ packages/client/src/components/Button.tsx
@@ -3,14 +3,20 @@ import '../styles/Button.styles.css';
 type PropsButton = {
   clickFunction: () => void;
   text: string;
-  svg?: React.ReactElement;
+  imgName?: string;
+  imgSize?: number;
+  imgAlt?: string;
 };
 
 const Button = (props: PropsButton) => {
   return (
     <button className="button_container quicksand-normal" onClick={props.clickFunction}>
       <p>{props.text}</p>
-      {props.svg}
+      <img
+        alt={props.imgAlt}
+        style={{ width: `${props.imgSize}`, height: `${props.imgSize}` }}
+        src={`../../public/${props.imgName}`}
+      />
     </button>
   );
 };
