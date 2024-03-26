13bdc3e981df867d2120e45766b1892df2a81a1a
ZoxDev <mathias.bar1809@gmail.com>
2024-03-26 20:17:19 +0100

    style: delete logo from the app

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -9,10 +9,6 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="bg-[#F8FAFC] dark:bg-[#0a1122] w-screen h-screen flex flex-col items-center justify-center">
-        <div className="logo">
-          <h1 className="quicksand-bold">Rossignol</h1>
-          <img src="/BIRD.svg"></img>
-        </div>
         <section className="app">
           <RossignolView />
         </section>
