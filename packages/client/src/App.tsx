8bd749e9b909bea4ed4f31efb5e97caa86eba1c6
ZoxDev <mathias.bar1809@gmail.com>
2024-03-29 16:53:52 +0100

    feat: adding back the logo & cahnge the color of rossignol

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -9,6 +9,10 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="bg-[#F8FAFC] dark:bg-[#0a1122] w-screen h-screen flex flex-col items-center justify-center">
+        <div className="flex gap-5 justify-center items-center">
+          <h1 className="text-bold text-2xl">Rossignol</h1>
+          <img src="/BIRD.svg" />
+        </div>
         <section className="app">
           <RossignolView />
         </section>
